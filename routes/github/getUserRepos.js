e437156e21c75d1ad2e8202d73869502dbc39b3c
Glitch (flashcoll-backend) <none>
2021-07-30 15:13:02 +0000

    📉🌲 Updated with Glitch

--- routes/github/getUserRepos.js
+++ routes/github/getUserRepos.js
@@ -5,7 +5,7 @@ module.exports = async function (fastify, opts) {
   console.log(process.env.GITHUB_PERSONAL_ACCESS_TOKEN);
   fastify.get('/user/:id/repos', async function (request, reply) {
     const octo = new Octokit({ auth: `${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}` });
-    const result = await octo.request("GET /user/{id}/repos", {
+    const result = await octo.request("GET /user/{id}/repos?per_page=100", {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
@@ -14,7 +14,8 @@ module.exports = async function (fastify, opts) {
       id: request.params.id
     });
     //return 'this is an example'
-    console.log(result);
+    //console.log(result.data);
+    console.log(result)
     return result.data;
   })
 }
